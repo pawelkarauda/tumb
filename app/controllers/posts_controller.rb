a3a0094a0ac7f7202335cb21219129844fd92fbd
Pawel Karauda <zlapmniehakerze@gmail.com>
2018-08-11 10:15:29 +0200

    Add form for new post, showing post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,19 @@ class PostsController < ApplicationController
     def new 
     end
 
-    
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+
+        redirect_to @post
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
 end
