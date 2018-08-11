5298b4163df6ef24f4a48e987d742da6d22b2911
Pawel Karauda <zlapmniehakerze@gmail.com>
2018-08-11 11:45:16 +0200

    Add valdiation to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
         @posts = Post.all.order('created_at DESC')
     end
 
-    def new 
+    def new
+        @post = Post.new
     end
 
     def show
@@ -12,9 +13,12 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new(post_params)
-        @post.save
 
-        redirect_to @post
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
     end
 
     private
